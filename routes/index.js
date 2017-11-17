1e33cedb1e0ed8f4a1a56cfd1a3ce2f9e787cefc
precioustosin <precioustosin@hotmail.com>
2017-11-17 20:20:57 +0100

    converted es5 code to es6, corrected lint errors, created index html file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import path from 'path';
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-module.exports = router;
+export default router;
