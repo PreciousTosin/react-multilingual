385f2020ec3b013a8fda3d3d70f164611f9b853e
precioustosin <precioustosin@hotmail.com>
2017-11-18 21:16:12 +0100

    configured webpack dev and hot middlewares

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+/* eslint import/no-extraneous-dependencies: 'off' */
 import express from 'express';
 import path from 'path';
 import favicon from 'serve-favicon';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import webpack from 'webpack';
+import config from './webpack.config';
 
 import index from './routes/index';
 import users from './routes/users';
 
 const app = express();
+const compiler = webpack(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -21,6 +25,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  quiet: false,
+  noInfo: false,
+  publicPath: config.output.publicPath,
+}));
+
+app.use(require('webpack-hot-middleware')(compiler, {
+  log: console.log,
+  path: '/__webpack_hmr',
+  heartbeat: 10 * 1000,
+}));
 
 app.use('/', index);
 app.use('/users', users);
