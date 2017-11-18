afec14f34399d1bed2009802b4fbb15705fc5f8c
precioustosin <precioustosin@hotmail.com>
2017-11-18 21:19:58 +0100

    created webpack config file and added project configurations to it

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+export default {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index',
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '../views/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx|.js?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, '../src'),
+        exclude: /node_modules/,
+        options: {
+          presets: ['env', 'react'],
+        },
+      },
+      {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: 'css-loader',
+            }),
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 },
+          },
+          'image-webpack-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['ts', '.js', '.jsx'],
+  },
+};
