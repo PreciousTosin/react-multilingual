cc7da141f9377ddf012c43b9ffb368ac0319e3c7
precioustosin <precioustosin@hotmail.com>
2017-12-01 00:58:51 +0100

    modified store with history and other middlewares

--- src/store.js
+++ src/store.js
@@ -1,7 +1,30 @@
+/* eslint import/no-extraneous-dependencies: 'off', class-methods-use-this: 'off',
+jsx-a11y/anchor-is-valid: 'off', max-len: 'off' */
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
 import content from './reducer';
 
-const createStore = require('redux').createStore;
+export const history = createHistory();
 
-const store = createStore(content);
+const initialState = {};
+const enhancers = [];
+const middleware = [
+  thunk,
+  routerMiddleware(history),
+];
+
+if (process.env.NODE_ENV === 'development') {
+  const { devToolsExtension } = window.devToolsExtension;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+
+const store = createStore(content, initialState, composedEnhancers);
 
 export default store;
