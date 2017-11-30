7feb5fd5c82d21653acbf520573c33bbe040af14
precioustosin <precioustosin@hotmail.com>
2017-11-30 18:37:43 +0100

    creater file called reducer.js to store state of app

---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+import actionTypes from './action-types';
+import api from './api';
+
+const initialState = {
+  content: api.getContent(), // Loads default language content (en) as an initial state
+};
+
+const reducer = function reduce(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.SWITCH_LANGUAGE:
+      return {
+        content: api.getContent(action.language),
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
