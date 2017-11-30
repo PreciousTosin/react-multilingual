42d3a7b92b4cf3318f1527f22e2fb36149b0d96a
precioustosin <precioustosin@hotmail.com>
2017-12-01 00:53:39 +0100

    modified switch component to use render instead of component

--- src/components/switch.jsx
+++ src/components/switch.jsx
@@ -1,4 +1,6 @@
-/* eslint import/no-extraneous-dependencies: 'off' */
+/* eslint import/no-extraneous-dependencies: 'off', react/jsx-max-props-per-line: off,
+react/jsx-first-prop-new-line: off */
+// import React, { Component } from 'react';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
@@ -6,12 +8,36 @@ import Home from './home';
 import About from './about';
 import Services from './services';
 
-const SwitchNavRoute = () => (
+const SwitchNavRoute = content => (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/services" component={Services} />
+    <Route exact path="/" render={props => (
+      <Home {...props} data={content} />
+    )}
+    />
+    <Route exact path="/about" render={props => (
+      <About {...props} data={content} />
+    )}
+    />
+    <Route exact path="/services" render={props => (
+      <Services {...props} data={content} />
+    )}
+    />
   </Switch>
 );
 
 export default SwitchNavRoute;
+
+
+/*
+class SwitchNavRoute extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+      </Switch>
+    );
+  }
+}
+*/
