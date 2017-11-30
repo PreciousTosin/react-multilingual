6f3da9fdee476ee4eda551025b429e203ff2cea7
precioustosin <precioustosin@hotmail.com>
2017-11-30 18:56:20 +0100

    changed functional component to class component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,45 @@
 /* eslint import/no-extraneous-dependencies: 'off', jsx-a11y/anchor-is-valid: 'off', max-len: 'off' */
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    const data = this.props.data;
+    const switchLanguage = this.props.switchLanguage;
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <span><Link className="navbar-brand" to="/">XML</Link></span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item"><Link className="nav-link" to="/about">About Us</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
+          </ul>
+
+          <ul className="navbar-nav navbar-right">
+            <li><Link to="#" onClick={switchLanguage.bind(this, 'en')}>EN</Link></li>
+            <li><Link to="#" onClick={switchLanguage.bind(this, 'lt')}>LT</Link></li>
+            <li><Link to="#" onClick={switchLanguage.bind(this, 'ru')}>RU</Link></li>
+          </ul>
+        </div>
+
+      </nav>
+    );
+  }
+}
+/*
 
 const Header = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -23,7 +62,14 @@ const Header = () => (
         <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
       </ul>
     </div>
+
+    <ul className="nav navbar-nav navbar-right">
+      <li><a href="#" data-target="#" onClick={switchLanguage.bind(this, 'en')}>EN</a></li>
+      <li><a href="#" data-target="#" onClick={switchLanguage.bind(this, 'lt')}>LT</a></li>
+      <li><a href="#" data-target="#" onClick={switchLanguage.bind(this, 'ru')}>RU</a></li>
+    </ul>
   </nav>
 );
+*/
 
 export default Header;
