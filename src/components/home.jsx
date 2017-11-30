835ef9a979e7b3a2d3947ba832e361bb33eab2a1
precioustosin <precioustosin@hotmail.com>
2017-12-01 00:52:41 +0100

    moved home to component folder, modified it to be functional component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,23 @@
 /* eslint import/no-extraneous-dependencies: 'off', jsx-a11y/anchor-is-valid: 'off' */
-import React, { Component } from 'react';
+import React from 'react';
 
+const Home = (content) => {
+  const data = content.data.data.content.page.home;
+  return (
+    <div className="jumbotron" id="home">
+      <div className="container">
+        <h1>{data.header}</h1>
+        <p>{data.text}</p>
+        <p><a className="btn btn-primary btn-lg" href="#" role="button">{data.goUp}</a></p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
+
+/*
 class Home extends Component {
   constructor() {
     super();
@@ -9,7 +26,7 @@ class Home extends Component {
   }
 
   render() {
-    const data = this.props.data;
+    const data = this.props.data.data.content.page.home;
     return (
       <div className="jumbotron" id="home">
         <div className="container">
@@ -21,5 +38,4 @@ class Home extends Component {
     );
   }
 }
-
-export default Home;
+*/
