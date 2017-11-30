8c268d181669a33e60a47cf9fe3f2bb756058a66
precioustosin <precioustosin@hotmail.com>
2017-12-01 00:46:51 +0100

    moved about to component folder, modified it to use functional component

--- src/about.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint import/no-extraneous-dependencies: 'off' */
-import React from 'react';
-
-function About() {
-  return (
-    <h1>About Us</h1>
-  );
-}
-export default About;
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,9 @@
 /* eslint import/no-extraneous-dependencies: 'off', jsx-a11y/anchor-is-valid: 'off' */
+// import React from 'react';
 import React from 'react';
 
-function About() {
-  const data = this.props.data;
+const About = (content) => {
+  const data = content.data.data.content.page.about;
   return (
     <div className="jumbotron" id="about">
       <div className="container">
@@ -14,5 +15,26 @@ function About() {
       </div>
     </div>
   );
-}
+};
+
 export default About;
+
+/*
+class About extends Component {
+  render() {
+    const data = this.props.data;
+
+    return (
+      <div className="jumbotron" id="about">
+        <div className="container">
+          <h1>{data.header}</h1>
+          <h3>{data.subheader}</h3>
+          <p>{data.text}</p>
+          <p>{data.forMore} <a href={data.url} data-target="blank">{data.url}</a></p>
+          <p><a className="btn btn-primary btn-lg" href="#" role="button">{data.goUp}</a></p>
+        </div>
+      </div>
+    );
+  }
+}
+*/
