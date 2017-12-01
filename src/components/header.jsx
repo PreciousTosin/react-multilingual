0b67e27198b6a6f68e79a24dbd058fcaa62df95b
precioustosin <precioustosin@hotmail.com>
2017-12-01 01:01:32 +0100

    modified header component to pure functional component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,56 @@
 /* eslint import/no-extraneous-dependencies: 'off', jsx-a11y/anchor-is-valid: 'off', max-len: 'off' */
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+const Header = (props) => {
+  const { switchLanguage } = props;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span><Link className="navbar-brand" to="/">XML</Link></span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item"><Link className="nav-link" to="/about">About Us</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
+        </ul>
+
+        <ul className="navbar-nav navbar-right">
+          <li><Link to="#" onClick={() => switchLanguage('en')}>EN</Link></li>
+          <li><Link to="#" onClick={() => switchLanguage('lt')}>LT</Link></li>
+          <li><Link to="#" onClick={() => switchLanguage('ru')}>RU</Link></li>
+        </ul>
+      </div>
+
+    </nav>
+  );
+};
+
+Header.propTypes = {
+  switchLanguage: PropTypes.func,
+};
+
+Header.defaultProps = {
+  switchLanguage: null,
+};
+
+export default Header;
+
+/*
 class Header extends Component {
   render() {
-    const data = this.props.data;
-    const switchLanguage = this.props.switchLanguage;
+    const { switchLanguage } = this.props;
 
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -29,9 +74,9 @@ class Header extends Component {
           </ul>
 
           <ul className="navbar-nav navbar-right">
-            <li><Link to="#" onClick={switchLanguage.bind(this, 'en')}>EN</Link></li>
-            <li><Link to="#" onClick={switchLanguage.bind(this, 'lt')}>LT</Link></li>
-            <li><Link to="#" onClick={switchLanguage.bind(this, 'ru')}>RU</Link></li>
+            <li><Link to="#" onClick={() => switchLanguage('en')}>EN</Link></li>
+            <li><Link to="#" onClick={() => switchLanguage('lt')}>LT</Link></li>
+            <li><Link to="#" onClick={() => switchLanguage('ru')}>RU</Link></li>
           </ul>
         </div>
 
@@ -39,37 +84,4 @@ class Header extends Component {
     );
   }
 }
-/*
-
-const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a><Link className="navbar-brand" to="/">XML</Link></a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item"><Link className="nav-link" to="/about">About Us</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
-      </ul>
-    </div>
-
-    <ul className="nav navbar-nav navbar-right">
-      <li><a href="#" data-target="#" onClick={switchLanguage.bind(this, 'en')}>EN</a></li>
-      <li><a href="#" data-target="#" onClick={switchLanguage.bind(this, 'lt')}>LT</a></li>
-      <li><a href="#" data-target="#" onClick={switchLanguage.bind(this, 'ru')}>RU</a></li>
-    </ul>
-  </nav>
-);
 */
-
-export default Header;
